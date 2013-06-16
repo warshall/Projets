d86c6688c64996d5b568297fa9329a3c53b0c6de
sat7070 <sat7070@gmail.com>
2013-06-16 22:37:15 +0200

    Insert Quit and Information

--- IA/QT/fenetre.cpp
+++ IA/QT/fenetre.cpp
@@ -1,12 +1,24 @@
 #include "fenetre.h"
-#include "fenetre.h"
+
 
 MaFenetre::MaFenetre() : QWidget()
 {
     setFixedSize(300, 100);
     ConstruireBoutons () ;
+    QObject::connect(Quitter, SIGNAL(clicked()), qApp, SLOT(quit())) ;
+    QObject::connect(Pierre, SIGNAL(clicked()), this, SLOT(BtnPierre())) ;
 
 }
+
+void MaFenetre::BtnPierre()
+{
+
+QMessageBox::information(this,tr("coucou"),tr("dddd")) ;
+
+}
+
+
+
 
 void MaFenetre::ConstruireBoutons()
 {
@@ -14,6 +26,7 @@ void MaFenetre::ConstruireBoutons()
     Pierre = new QPushButton ("Pierre", this);
     Papier = new QPushButton("Papier", this);
     Ciso = new QPushButton("Ciso", this);
+    Quitter = new QPushButton ("Quitter", this) ;
 
     lblJoueur = new QLabel(this) ;
     lblJoueur->setText("Joueur: 0");
@@ -25,4 +38,5 @@ void MaFenetre::ConstruireBoutons()
     Pierre->move(10, 50) ;
     Papier->move(110, 50) ;
     Ciso->move(210, 50) ;
+    Quitter->move(210,10) ;
 }
--- IA/QT/fenetre.h
+++ IA/QT/fenetre.h
@@ -4,22 +4,30 @@
 /*#include <QApplication>
 #include <QWidget>
 #include <QPushButton>
-#include <QLabel>*/
-
+#include <QLabel>
+#include <QMessageBox>*/
 #include <QtGui>
 
 class MaFenetre : public QWidget // On hérite de QWidget (IMPORTANT)
 {
+   Q_OBJECT
+
     public:
     MaFenetre();
 
+    public slots:
+      void BtnPierre() ;
+
+
     private:
-    void ConstruireBoutons ();
-    QPushButton *Pierre;
-    QPushButton *Papier;
-    QPushButton *Ciso;
-    QLabel *lblJoueur;
-    QLabel *lblPC;
+    void ConstruireBoutons () ;
+    QPushButton *Pierre ;
+    QPushButton *Papier ;
+    QPushButton *Ciso ;
+    QPushButton *Quitter ;
+    QLabel *lblJoueur ;
+    QLabel *lblPC ;
+
 };
 
 
